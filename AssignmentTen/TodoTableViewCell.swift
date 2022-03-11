4b2982e38ee32664e7d5cf0287b0eb8c8210cf43
karma <karmatashi954@gmail.com>
2022-03-11 09:22:36 +0545

    Initial Commit

---
+++ AssignmentTen/TodoTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  TodoTableViewCell.swift
+//  AssignmentTen
+//
+//  Created by karma on 3/11/22.
+//
+
+import UIKit
+
+class TodoTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nameLbl: UILabel!
+   
+    
+
+}
